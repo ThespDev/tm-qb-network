2b0a0da5b8eb18f37500abc530338d8ab53f63ca
schmilly <william.f.s.bake@gmail.com>
2023-05-12 17:38:39 +0800

    C99 q. now in CSV; Also Modified Questions Answer parsing program

--- QB/Write_Questions_And_Answers_Function.c
+++ QB/Write_Questions_And_Answers_Function.c
@@ -1,25 +1,33 @@
+// USAGE: write_question_and_answer <filepath> <languageused>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+const char Usage_msg[] = ("USAGE: ./Write_Questions <file path> <Langauge used>\n");
+
+void usage(){
+  printf("\n%s\n",Usage_msg);
+  exit(EXIT_FAILURE);
+}
+
 int main(int argc, char *argv[]) {
    FILE *fp;
-   char filename[20];
+   char filename[20]; //track name of file variable
+   char Language[20]; //track Language
 
    // get filename from server request
-   strcpy(filename, argv[1]);
-
-   // open appropriate file based on filename
-   if(strcmp(filename, "C-Programming_Questions.txt") == 0) { // if server requests C-Programming_Questions.txt
-      fp = fopen("C-Programming_Questions.txt", "r"); // open C-Programming_Questions.txt for reading
-   } else if(strcmp(filename, "JAVA_Programming_Questions.txt") == 0) { // if server requests JAVA_Programming_Questions.txt
-      fp = fopen("JAVA_Programming_Questions.txt", "r"); // open JAVA_Programming_Questions.txt for reading
-   } else if(strcmp(filename, "file3.txt") == 0) { // if server requests Python_Programming_Questions.txt
-      fp = fopen("Python_Programming_Questions.txt", "r"); // open Python_Programming_Questions.txt for reading
-   } else { // if server requests an invalid filename
-      printf("Invalid filename\n"); // print error message
-      exit(1); // exit program with error code
+   if (argc != 3 ) {
+    printf("Error too many or too little arguments\n Current argument count: %i",argc); usage();
    }
+   strcpy(filename, argv[1]);
+   strcpy(Language, argv[2]);
+   fp = fopen(filename, "r"); // open file for reading
+   if (fp == NULL){
+    printf("Internal Error: Unable to open file %s, file may be missing",filename); usage(); 
+   }
+
+}
 
 // function to write a question and its answer to a file
 void write_question_and_answer(char* file_path, char* question, char* answer){
@@ -55,55 +63,15 @@ void read_random_question_and_answer(char* file_path){
         }
     }
     printf("%s%s\n", question, answer);
-    fclose(file);
-}
-
-    // loop through the file to find multiple-choice questions and answers
-    while (fgets(line, sizeof(line), file)) {
-        if (strstr(line, "MC: ") != NULL) {
-            multiple_choice_found = 1;
-            answer_found = 0;
-            if (rand() % ++count == 0) {
-                strcpy(question, line + 3);
-                fgets(line, sizeof(line), file);
-                strcpy(answer, line + 3);
-            }
-        } else if (strstr(line, "MCA: ") != NULL) {
-            answer_found = 1;
-            question_found = 0;
-        }
-    }
-    printf("%s%s\n", question, answer);
     fclose(file); // close the file
 }
 
 void search_and_print(char* file_path, char* keyword){
     FILE* file = fopen(file_path, "r");
     char line[1000];
-    while (fgets(line, sizeof(line), file)) {nclude <stdio.h>
-#include <stdlib.h>
-
-int main() {
-   FILE *fp;
-
-   // Open the file
-   fp = fopen("file.txt", "w+");
-
-   // Check if the file is opened successfully
-   if (fp == NULL) {
-      printf("Error opening the file.\n");
-      exit(1);
-   }
-        if (strstr(line, keyword) != NULL) {
-            printf("%s", line);
-            while (fgets(line, sizeof(line), file)) {
-                if (isspace(line[0])) {
-                    break;
-                } else {
-                    printf("%s", line);
-                }
-            }
-        }
+    while (fgets(line, sizeof(line), file)) {
+    #include <stdio.h>
+    #include <stdlib.h>
     }
-    fclose(file); // close the file
 }
+
