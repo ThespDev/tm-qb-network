a9e07b6ecd65ec1afa8fbf1f1b95a66e79f12a67
ThespDev <pablonaydenovau@gmail.com>
2023-05-12 15:41:03 +0800

    basic login

--- TM/server.py
+++ TM/server.py
@@ -10,7 +10,14 @@ host = 'localhost'
 HTTP_PORT = 9001
 QB_PORT = 9002
 sel = selectors.DefaultSelector()
+def verify_user(username,password):
+    f = open('users.json')
+    data = json.load(f)
 
+    if username in data and data[username] == password:
+        return True
+    else:
+        return False
 
 def accept(sock):
     conn, addr = sock.accept()  # Should be ready
@@ -23,31 +30,59 @@ def accept(sock):
 def service_connection(key, mask):
     sock = key.fileobj
     data = key.data
-    out = b""
+    serve_standard = False
+    incorrectlogin = False
+    question = False
+
     if mask & selectors.EVENT_READ:
+
         recv_data = sock.recv(1024)
-        if recv_data:
-            out += recv_data
+        print(recv_data.decode())
+        response = recv_data.decode().split('\n')
+        rq_type = response[0]
+        print(rq_type[4:10])
+        if rq_type[0:3] == "GET" and rq_type[4:6] == "/ ":
+            serve_standard = True
+        elif rq_type[0:3] == "GET" and rq_type[4:10] == "/login":
+            creds = (rq_type.split(' ')[1])
+            user = creds.split('=')[1].split('&')[0]
+            password = creds.split('=')[2]
+            verify = verify_user(user,password)
+            print(verify)
+            if verify:
+                question = True
+            else:
+                incorrectlogin = True
+
+
+            print(user + " " + password)
+            print("login attempted")
+                
+    
+    if mask & selectors.EVENT_WRITE:
+        
+        if serve_standard:
+            response = 'HTTP/1.0 200 OK\n\n' + loginHTML    
+            sock.send(response.encode())
+        elif incorrectlogin:
+            response = 'HTTP/1.0 200 OK\n\n' + loginHTML    
+            sock.send(response.encode())
+        elif question:
+            response = 'HTTP/1.0 200 OK\n\n' + multiHTML
+            sock.send(response.encode())
+        sel.unregister(sock)
+        sock.close()
     else:
         print(f"Closing connection to {data}")
         sel.unregister(sock)
         sock.close()
-    if mask & selectors.EVENT_WRITE:
-        response = 'HTTP/1.0 200 OK\n\n' + loginHTML
-        sock.send(response.encode())
 
-
-def verify_user(username,password):
-    f = open('users.json')
-    data = json.load(f)
-
-    if username in data and data[username] == password:
-        return True
-    else:
-        return False
-
-with open("Question_Layout.html","r") as login_page:
+with open("login_page.html","r") as login_page:
     loginHTML = login_page.read()
+
+with open("multi_choice.html","r") as multi_page:
+    multiHTML = multi_page.read()
+    
 
 
 # Create socket
@@ -71,7 +106,6 @@ while True:
     events = sel.select(timeout=None)
     for key,mask in events:
         if key.data is None:
-            print("accepting")
             accept(key.fileobj)
         else:
             service_connection(key,mask)
