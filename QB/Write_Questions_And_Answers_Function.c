d754e2c37f95191f81d5f30412070d1034b7f897
schmilly <william.f.s.bake@gmail.com>
2023-05-13 18:44:09 +0800

    Still trying to get CSV parsing to work; Slow progress

--- QB/Write_Questions_And_Answers_Function.c
+++ QB/Write_Questions_And_Answers_Function.c
@@ -1,16 +1,64 @@
 // USAGE: write_question_and_answer <filepath> <languageused>
 
+#define MAXLINELENGTH 1024
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-const char Usage_msg[] = ("USAGE: ./Write_Questions <file path> <Langauge used>\n");
+//structure definitons probably going to have to use in diff program
+struct multi_choiceq {
+  int qnum;
+  char* qtext;
+  char* options[4]; //2d array
+  int answer; //1 = a, 2 = b, 3 = c, 4 = d e.t.c. 
+};
+
+struct programq {
+  int qnum;
+  char* qtext;
+  char* lang; //C99, Java or Python
+  char* choseninputs[3];    //What is expected to go in
+  char* expectedoutputs[3]; //What is expected to come out 
+};
+
+const char Usage_msg[] = ("USAGE: ./Write_Questions <file path> <Langauge used>");
 
 void usage(){
   printf("\n%s\n",Usage_msg);
   exit(EXIT_FAILURE);
 }
 
+
+char* getfield(char* line, int num)
+{
+    char* tok;
+    for (tok = strtok(line, ",");
+            tok && *tok;
+            tok = strtok(NULL, ",\n"))
+    {
+        if (!--num)
+            return tok;
+    }
+    return NULL;
+}
+
+char* getfieldarrow(char* line, int num)
+{
+    char* tok;
+    for (tok = strtok(line, ">");
+            tok && *tok;
+            tok = strtok(NULL, ">\n"))
+    {
+        if (!--num)
+            return tok;
+    }
+    return NULL;
+}
+
+
+
+//int fileparser(filepath,language) { 
+// using as main for now, but shouldn't be starting place for server, better to call it as a function
 int main(int argc, char *argv[]) {
    FILE *fp;
    char filename[20]; //track name of file variable
@@ -24,10 +72,47 @@ int main(int argc, char *argv[]) {
    strcpy(Language, argv[2]);
    fp = fopen(filename, "r"); // open file for reading
    if (fp == NULL){
-    printf("Internal Error: Unable to open file %s, file may be missing",filename); usage(); 
+    printf("Internal Error: Unable to open file %s",filename); usage(); 
    }
+  
+  char line[MAXLINELENGTH];
+  
+  struct multi_choiceq multiq[200];
+  struct programq codeq[200];
+  int MCA_Counter = 0;
+  int Code_Counter = 0;
 
+  while (fgets(line, 1024, fp))
+    {
+        char* tmp = strdup(line);
+
+        //skip commented lines (lines start with hashes)
+        if (tmp[0] == '#'){
+          continue;
+        }
+
+        printf("Field 2 would be %s\n", getfield(tmp, 2));
+        // NOTE strtok clobbers tmp
+        
+        //This if command segfaults the program IDK why
+      //Removed when fixed
+        if (strcmp(getfield(tmp,2),"MCA")){
+          multiq[MCA_Counter].qnum = atoi(getfield(tmp,1));
+          multiq[MCA_Counter].qtext = getfield(tmp,3);
+          for(int x = 0; x < 4; x++){
+            multiq[MCA_Counter].options[x] = getfieldarrow(getfield(tmp,4),x);
+          }
+          multiq[MCA_Counter].answer = atoi(getfield(tmp,5));
+        
+          printf("Parsed Line \n, Number: %i, Text:, %s, and Answer of:%i\n",multiq[MCA_Counter].qnum,multiq[MCA_Counter].qtext,multiq[MCA_Counter].answer);
+
+
+          MCA_Counter++;
+        }
+        free(tmp);
+    }
 }
+
 
 // function to write a question and its answer to a file
 void write_question_and_answer(char* file_path, char* question, char* answer){
